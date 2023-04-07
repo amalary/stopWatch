379698773a1fc02c2b829c4dd91fc1498b0de1b6
Anthony Malary <amalary@bu.edu>
2023-04-07 12:57:51 -0700

    Styling added and logic being added:

---
+++ script/app.js
@@ -0,0 +1,11 @@
+let seconds = 00;
+let tens = 00;
+
+let getSeconds = document.querySelector(".seconds");
+let getTens = document.querySelector(".tens");
+
+let startButton = document.querySelector(".start");
+let stopButton = document.querySelector(".stop");
+let restButton = document.querySelector(".reset");
+
+
