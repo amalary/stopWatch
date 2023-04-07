eb08acd588f4ac67a6ce57aedbf47b9ecd9040ee
Anthony Malary <amalary@bu.edu>
2023-04-07 14:21:56 -0700

    Have to fix tens portion of code

--- script/app.js
+++ script/app.js
@@ -1,11 +1,32 @@
+
 let seconds = 00;
 let tens = 00;
 
 let getSeconds = document.querySelector(".seconds");
 let getTens = document.querySelector(".tens");
-
 let startButton = document.querySelector(".start");
 let stopButton = document.querySelector(".stop");
 let restButton = document.querySelector(".reset");
+
+startButton.addEventListener('click', ()=>{
+  tens++;
+  if(tens <= 9){
+    getTens.innerHTML = '0' + tens;
+  }
+
+  if(tens > 9){
+    getTens.innerHTML = tens
+  }
+
+  if(tens>99){
+    seconds++;
+    getSeconds.innerHTML = '0' + seconds;
+    tens = 0;
+    getTens.innerHTML = '0' + 0;
+  }
+  if(seconds > 9){
+    getSeconds.innerHTML = seconds;
+  }
+})
 
 
